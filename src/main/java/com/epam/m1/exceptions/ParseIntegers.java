d19fc8340c18c8b0dd9e50319bebde4c9206ff05
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-24 11:40:45 +0300

    feat: complete task1 module1 stage1 v.0

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -3,6 +3,7 @@ package com.epam.m1.exceptions;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Complete the code, parse integers, calculate the sum of numbers in the WORDS, join strings with
@@ -21,8 +22,12 @@ public class ParseIntegers {
         String justWords = "";
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
-            // todo: complete it
+            try {
+                int number = Integer.parseInt(next);
+                sum += number;
+            } catch (NumberFormatException e) {
+                justWords = justWords + " " + next;
+            }
         }
         System.out.println("Sum is " + sum);
         System.out.println("Just words:" + justWords);
